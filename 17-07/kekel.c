d02ec4c14b375f2722e2ee32e248de1c50c4a233
Júlia Carneiro <83188447+julia-carneiro@users.noreply.github.com>
2024-07-17 17:37:44 -0300

    7seg + vida + pista menor + add gasolina

---
+++ 17-07/kekel.c
@@ -0,0 +1,148 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <asm/io.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/uaccess.h>
+
+#define DATA_A 0x80
+#define DATA_B 0x70
+#define LW_BRIDGE_BASE 0xFF200000
+#define LW_BRIDGE_SPAN 0x00005000
+#define START 0xc0
+#define WR_FULL 0xb0
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("TESTE");
+
+// Declara ponteiros para endereços virtuais dos registradores
+volatile int *ptr_A; 
+volatile int *ptr_B;
+volatile int *ptr_FULL;
+volatile int *ptr_START;
+
+// Endereço virtual base para o light-weight bridge
+void *HW_virtual; 
+
+// Define uma estrutura para o dispositivo virtual
+struct device
+{
+    char data[100];
+
+} virtual_device;
+
+struct cdev *mcdev; // Estrutura do dispositivo de caractere
+int major_number; // Número major atribuído ao nosso dispositivo
+ssize_t ret; // Valor de retorno usado nas funções
+
+dev_t dev_num; // Número do dispositivo
+
+#define DEVICE_NAME "GPU" // Nome do nosso dispositivo
+
+// Função de abertura do dispositivo
+int device_open(struct inode *inode, struct file *filp)
+{
+    printk(KERN_INFO "GPU: opened device");
+    return 0;
+}
+
+// Função de escrita do dispositivo
+static ssize_t device_write(struct file *filp, unsigned long long *bufSourceData, size_t bufCount, loff_t *curOffSet)
+{
+    printk(KERN_INFO "GPU: writing device");
+    ret = copy_from_user(virtual_device.data, bufSourceData, bufCount);
+
+    // Espera enquanto o buffer estiver cheio
+    // while (*ptr_FULL)
+    // {
+    //     printk(KERN_INFO "fila cheia");
+    // }
+
+    // Extrai dados para escrever nos registradores de hardware
+    unsigned long int dataA;
+    unsigned long int dataB;
+    memcpy(&dataA, virtual_device.data, 4);
+    memcpy(&dataB, virtual_device.data + 4, 4);
+
+    // Escreve dados nos registradores
+    *ptr_A = dataA;
+    *ptr_B = dataB;
+    *ptr_START = 1; 
+    *ptr_START = 0;
+
+    return ret;
+}
+
+// Função de leitura do dispositivo
+ssize_t device_read(struct file *filp, char *bufStoreData, size_t bufCount, loff_t *curOffSet)
+{
+    printk(KERN_INFO "GPU: reading from device");
+    ret = copy_to_user(bufStoreData, virtual_device.data, bufCount);
+    return ret;
+}
+
+// Função de fechamento do dispositivo
+static int device_close(struct inode *inode, struct file *filp)
+{
+    printk(KERN_ALERT "GPU: unloaded module");
+}
+
+// Define as operações do dispositivo
+struct file_operations fops = {
+    .owner = THIS_MODULE,
+    .open = device_open,
+    .release = device_close,
+    .write = device_write,
+    .read = device_read
+};
+
+// Função de inicialização do módulo
+static int __init iniciar(void)
+{
+    // Aloca número major para o dispositivo
+    ret = alloc_chrdev_region(&dev_num, 0, 1, DEVICE_NAME);
+    if (ret < 0)
+    {
+        printk(KERN_ALERT "GPU: failed to allocate major number");
+        return ret;
+    }
+
+    // Mapeia a memória do hardware
+    HW_virtual = ioremap_nocache(LW_BRIDGE_BASE, LW_BRIDGE_SPAN);
+
+    // Define os ponteiros para os registradores de hardware
+    ptr_A = (int *)(HW_virtual + DATA_A);
+    ptr_B = (int *)(HW_virtual + DATA_B);
+    ptr_START = (int *)(HW_virtual + START);
+    ptr_FULL = (int *)(HW_virtual + WR_FULL);
+
+    major_number = MAJOR(dev_num);
+    printk(KERN_INFO "GPU: major number is %d", major_number);
+    printk(KERN_INFO "\tuse \"mknod /dev/%s c %d 0\" for device file", DEVICE_NAME, major_number);
+
+    mcdev = cdev_alloc();
+    mcdev->ops = &fops;
+    mcdev->owner = THIS_MODULE;
+
+    // Adiciona o dispositivo de caractere ao kernel
+    ret = cdev_add(mcdev, dev_num, 1);
+    if (ret < 0)
+    {
+        printk(KERN_ALERT "GPU: unstruct file *filpable to add dev cdev to kernel");
+        return ret;
+    }
+}
+
+// Função de saída do módulo
+static void __exit sair(void)
+{
+    iounmap(HW_virtual); // Desmapeia a memória
+    cdev_del(mcdev); // Remove o dispositivo de caractere
+    unregister_chrdev_region(dev_num, 1); // Desregistra o número do dispositivo
+    printk(KERN_INFO "Fechando o modulo");
+}
+
+// Define as funções de inicialização e saída do módulo
+module_init(iniciar);
+module_exit(sair);
---
+++ 17-07/main.c
@@ -0,0 +1,429 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h> 
+#include <linux/input.h>
+#include "biblioteca.c"
+#include "sete_seg.c"
+#include "cenario.c"
+
+
+struct input_event event;
+int mouse_x,mouse_y;
+int pos_x = 300;
+int pos_y = 400;
+int pos_y_tiro,click,pos_x_tiro; 
+int limites_x[2] = {120,490};
+int limites_y[2] = {0,460};
+
+int verificar_limite(int limite[], int valor_atual, int valor_mouse);
+
+void *movimento_mouse(){
+  int fd = open("/dev/input/event0", O_RDONLY);
+  if(fd == -1){
+      printf("Nao foi possivel abrir o arquivo!\n");
+  }
+
+  while (1) {
+    read(fd, &event, sizeof(struct input_event));
+
+    if(event.type== EV_REL && event.code == REL_X){
+        mouse_x = event.value;
+        pos_x = verificar_limite(limites_x, pos_x, mouse_x);
+    }
+
+    if(event.type== EV_REL && event.code == REL_Y){
+        mouse_y = event.value;
+        pos_y = verificar_limite(limites_y, pos_y, mouse_y);
+    }
+
+    if (event.type == EV_KEY && event.code == BTN_LEFT)
+    {
+      if (event.value == 1) {
+        click = 1;
+      }
+      else{
+        click = 0;
+      }
+    }
+  }
+  
+  close(fd);
+  return NULL;
+
+}
+
+int verificar_limite(int limite[], int valor_atual, int valor_mouse){
+  // Verifica se o movimento é maior que o limite inferior e menor que o limite superior
+  if(valor_atual + valor_mouse >= limite[0] && valor_atual + valor_mouse <= limite[1]){
+    return valor_atual + valor_mouse;
+  }
+  else if(valor_atual + valor_mouse < limite[0]){
+    return limite[0];
+  }
+  else{
+    return limite[1];
+  }
+}
+
+
+int randomInt() {
+  return 130 + rand() % (460 - 100 + 1);
+}
+
+int pontuacao(int pontos){
+  int uni = pontos %10;
+  int dez = (pontos / 10) % 10;
+  int cem = (pontos / 100) % 10;
+
+  ligar_7seg(cem,4);
+  ligar_7seg(dez,3);
+  ligar_7seg(uni,2);
+}
+
+
+
+
+
+int mostrar_gasosa(int gasosa[]){
+
+    wbr02(520, 10, 0, gasosa[4], 11);
+    wbr02(540, 10, 0,gasosa[3] , 12);  
+    wbr02(560, 10, 0, gasosa[2], 13);
+    wbr02(580, 10, 0, gasosa[1],14);
+    wbr02(600, 10, 0, gasosa[0], 15);
+       
+      
+      
+  }
+
+
+  // switch (gasosa)
+  // {
+  //   case 100:
+  //     wbr02(520, 10, 0, 1, 11);
+  //     wbr02(540, 10, 0, 1, 12);
+  //     wbr02(560, 10, 0, 1, 13);
+  //     wbr02(580, 10, 0, 1, 14);
+  //     wbr02(600, 10, 0, 1, 15);
+  //   break;
+
+  //   case 80:
+  //     wbr02(520, 10, 0, 1, 11);
+  //     wbr02(540, 10, 0, 1, 12);
+  //     wbr02(560, 10, 0, 1, 13);
+  //     wbr02(580, 10, 0, 1, 14);
+  //     wbr02(600, 10, 0, 0, 15);
+  //   break;
+
+  //   case 60:
+  //     wbr02(520, 10, 0, 1, 11);
+  //     wbr02(540, 10, 0, 1, 12);
+  //     wbr02(560, 10, 0, 1, 13);
+  //     wbr02(580, 10, 0, 0, 14);
+  //     wbr02(600, 10, 0, 0, 15);
+  //   break;
+
+  //   case 40:
+  //     wbr02(520, 10, 0, 1, 11);
+  //     wbr02(540, 10, 0, 1, 12);
+  //     wbr02(560, 10, 0, 0, 13);
+  //     wbr02(580, 10, 0, 0, 14);
+  //     wbr02(600, 10, 0, 0, 15);
+  //   break;
+
+  //   case 20:
+  //     wbr02(520, 10, 0, 1, 11);
+  //     wbr02(540, 10, 0, 0, 12);
+  //     wbr02(560, 10, 0, 0, 13);
+  //     wbr02(580, 10, 0, 0, 14);
+  //     wbr02(600, 10, 0, 0, 15);
+  //   break;
+
+  //   case 0:
+  //     wbr02(520, 10, 0, 0, 11);
+  //     wbr02(540, 10, 0, 0, 12);
+  //     wbr02(560, 10, 0, 0, 13);
+  //     wbr02(580, 10, 0, 0, 14);
+  //     wbr02(600, 10, 0, 0, 15);
+  //   break;
+
+  // default:  
+  // break;
+  // }  
+  
+
+
+
+
+int mostrar_vida(int vida){
+
+  switch (vida)
+  {
+    case 0:
+      wbr02(10, 10, 15, 0, 8);
+      wbr02(31, 10, 15, 0, 9);
+      wbr02(52, 10, 15, 0, 10);
+    break;
+
+    case 1:
+      wbr02(31, 10, 15, 0, 9);
+    break;
+
+    case 2:
+      wbr02(52, 10, 15, 0, 10);
+    break;
+
+  default:
+    break;
+  }  
+  
+
+}
+
+
+
+int main() {
+
+  //Inicia as vida 
+  wbr02(10, 10, 15, 1, 8);
+  wbr02(31, 10, 15, 1, 9);
+  wbr02(52, 10, 15, 1, 10);
+
+  int pontos = -1;
+  // X = 640 Y = 480
+  // O pixel de erferencia da sprite é o superior esquerdo
+  // Sempre subtrair/adicionar 10 para centralizar, e 20 para limitar os cantos1
+  // Ex: Canto inferior direito X = 620 Y = 460
+  
+  pthread_t thread_mouse;
+  if(pthread_create(&thread_mouse, NULL, movimento_mouse, NULL)){
+    printf("Erro ao criar thread.");
+  }
+
+  gerar_cenario();  
+
+  while(1){
+
+
+    srand(time(NULL));
+    int random_x_position;
+
+    // Variáveis das Sprites dos Carros
+    int vetor_pos_x[4];
+    int vetor_pos_y[4]= {0,0,0,0};
+    int vetor_criado_y[4] = {0,0,0,0};
+
+    //Variáveis Sprite Gasolina
+    int pos_x_gasolina = 0;
+    int pos_y_gasolina = 0;
+    int gasolina_criada = 0;
+
+    //Variáveis Sprite Pontos
+
+    // Variavéis dos Jogo
+    int tempo = 0;
+    int vida = 3;
+    int gasolina = 100;
+    int velocidade = 1;
+
+    // Posição inicial do Mouse
+    pos_x = 300;
+    pos_y = 400;
+    //cotrola o mostrador de gasolina
+    int contador_gasolina = 5;
+    int vet_gasolina[] = {1,1,1,1,1};
+    
+    // Inicia sprite do Carro
+    wbr02(pos_x, pos_y, 18, 1, 1);
+    
+    while(vida && gasolina){
+      // Movimenta Sprite Mouse   
+      wbr02(pos_x, pos_y, 18, 1, 1);
+
+      // Define quando as Sprite vão ser geradas
+      tempo++;
+      if(tempo % 1000 == 0){
+        velocidade++;
+      }
+      
+      //Move o Tiro
+      if(pos_y_tiro == 0){
+        wbr02(pos_x_tiro, pos_y_tiro, 11, 0, 6);
+      }
+      else{
+        pos_y_tiro-=5;
+        wbr02(pos_x_tiro, pos_y_tiro, 11, 1, 6);
+      }
+
+      // Atira se houver clique e não houver um tiro já gerado
+      if(click && pos_y_tiro <= 0){
+        pos_y_tiro = pos_y-19;
+        pos_x_tiro = pos_x;
+        wbr02(pos_x_tiro, pos_y_tiro, 11, 1, 6);
+      }
+
+      //Define posição aleatoria do carro
+      for (int i = 0;i<4;i++){
+          if(vetor_pos_y[i] >= 460){
+            random_x_position = randomInt();
+            vetor_pos_x[i] = random_x_position;
+            vetor_pos_y[i] = 0;
+          }
+      }
+
+      // Define posição aleatória Gasolina
+      if(pos_y_gasolina == 0 && gasolina_criada == 0){
+        pos_x_gasolina = randomInt();
+      }
+
+      //Gera Sprite Gasolina
+      if(tempo % 350 == 0 && gasolina_criada == 0){
+        gasolina_criada = 1;
+        wbr02(pos_x_gasolina, pos_y_gasolina, 0, 1, 7);
+      }
+
+      //Movimenta Sprite Gasolina
+      if(gasolina_criada){
+        if(pos_y_gasolina >= 460){
+          gasolina_criada = 0;
+          
+        }
+        else{
+          pos_y_gasolina+=velocidade;
+          wbr02(pos_x_gasolina, pos_y_gasolina, 0, 1, 7);
+        }
+      }else{
+        pos_y_gasolina = 0; 
+        wbr02(pos_x_gasolina, pos_y_gasolina, 0, 0, 7);
+      }
+
+      // Consome gasolina
+      if(tempo % 500 == 0){
+        gasolina-=20;
+        vet_gasolina[contador_gasolina] = 0;
+        contador_gasolina -=1;
+        printf("%d\n", gasolina); 
+      }
+
+      // Colisão Carro - Gasolina
+      if((pos_x + 20 > pos_x_gasolina && pos_x +20 < pos_x_gasolina+ 20 ) || (pos_x > pos_x_gasolina && pos_x < pos_x_gasolina + 20)){
+        if((pos_y >pos_y_gasolina && pos_y < pos_y_gasolina + 20)|| (pos_y +20 > pos_y_gasolina  && pos_y +20 < pos_y_gasolina+20)){
+          if(gasolina < 100){
+            gasolina+=20;
+            contador_gasolina +=1;
+            vet_gasolina[contador_gasolina]=1;
+          }
+          gasolina_criada = 0;
+        
+          printf("Pegou!: %d\n", gasolina); 
+        } 
+      }
+      mostrar_gasosa(vet_gasolina);
+
+      //Colisão Gasolina - Tiro
+      if((pos_x_tiro + 10 >= pos_x_gasolina && pos_x_tiro + 9 <= pos_x_gasolina+20) || 
+      (pos_x_tiro+9>=pos_x_gasolina && pos_x_tiro+10<=pos_x_gasolina+20)){
+        if (pos_y_tiro <= pos_y_gasolina + 19 && pos_y_tiro >= pos_y_gasolina){
+          gasolina_criada = 0;
+          pos_y_tiro = 0;
+        }
+      }
+
+      //Cria os carros
+      if(!vetor_criado_y[0] || !vetor_criado_y[1] || !vetor_criado_y[2] || !vetor_criado_y[3]){
+
+          if(tempo == 10 && vetor_criado_y[0] == 0){
+              
+              wbr02(vetor_pos_x[0], vetor_pos_y[0], 19, 1, 5);
+              vetor_criado_y[0]=1;
+          }
+          if(tempo == 200 && vetor_criado_y[1] == 0){
+              wbr02(vetor_pos_x[1], vetor_pos_y[1], 19, 1, 2);
+              vetor_criado_y[1]=1;
+              
+          }
+          if(tempo == 100 && vetor_criado_y[2] == 0){
+          
+              wbr02(vetor_pos_x[2], vetor_pos_y[2], 19, 1, 3);
+              vetor_criado_y[2]=1;
+              
+          }
+          if(tempo == 350 && vetor_criado_y[3] == 0){
+              wbr02(vetor_pos_x[3], vetor_pos_y[3], 19, 1, 4);
+              vetor_criado_y[3]=1;
+              
+          }
+      }
+      
+      // Define a próxima posição das Sprites dos Carros em Y
+      for(int j = 0; j<4; j++){
+          //Verifica carro criado 
+          if(vetor_criado_y[j]==1){
+              vetor_pos_y[j]+= velocidade;
+          }
+      }  
+      
+      // Movimenta Sprites dos Carros
+      for (int k = 0; k < 4; k++){
+        wbr02(vetor_pos_x[k], vetor_pos_y[k], 19, vetor_criado_y[k], 2+k);
+      }
+      
+      //Colisão carro - carro 
+      for (int l = 0; l < 4; l++){
+        if((pos_x + 20 > vetor_pos_x[l] && pos_x +20 < vetor_pos_x[l]+ 20 ) || (pos_x > vetor_pos_x[l] && pos_x < vetor_pos_x[l] + 20)){
+          if((pos_y >vetor_pos_y[l] && pos_y < vetor_pos_y[l] + 20)|| (pos_y +20 > vetor_pos_y[l]  && pos_y +20 < vetor_pos_y[l]+20)){
+            wbr01(7, 0, 0);
+            vetor_pos_y[l] = 460;
+            vida--;
+            wbr01(3, 3, 3);
+          }
+        }
+      }
+      
+      //Mostra vida
+      ligar_7seg(vida, 5);
+      mostrar_vida(vida);
+
+
+
+      int colidiu = 0; 
+
+      //Colisão Carro - Tiro
+      for (int l = 0; l < 4; l++){
+        if((pos_x_tiro + 10 >= vetor_pos_x[l] && pos_x_tiro + 9 <= vetor_pos_x[l]+20) || 
+        (pos_x_tiro+9>=vetor_pos_x[l] && pos_x_tiro+10<=vetor_pos_x[l]+20)){
+          if (pos_y_tiro <= vetor_pos_y[l] + 19 && pos_y_tiro >= vetor_pos_y[l]){
+            vetor_pos_y[l] = 460;
+            pos_y_tiro = 0;
+            colidiu = 1;
+          }
+        }
+      }
+      
+      if (colidiu == 1){
+        pontos += 1;
+        colidiu = 0;  
+      }
+
+      pontuacao(pontos);
+  
+    }
+    // Tela Game Over
+    printf("Game-Over\n");
+    pontos = 0;
+
+
+    //Inicia as vida 
+    wbr02(10, 10, 15, 1, 8);
+    wbr02(31, 10, 15, 1, 9);
+    wbr02(52, 10, 15, 1, 10);
+
+
+  }
+
+  pthread_join(thread_mouse,NULL);
+  return 0;
+}
